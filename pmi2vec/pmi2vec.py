8eb87c296442e38daad5ba736bcaddc5587f1bc5
Aleksi Sahala <aleksi.sahala@helsinki.fi>
2019-03-17 18:10:16 +0200

    Added measures etc.

--- pmi2vec/pmi2vec.py
+++ pmi2vec/pmi2vec.py
@@ -9,14 +9,14 @@ by using pointwise mutual information and singular
 value decomposition.
 
 ================================================= """
-
+import time
 import sys
+import re
 import itertools
 import math
 import json
 import gzip
 import numpy as np
-import dictionary
 from numpy.linalg import norm
 from collections import Counter
 from sklearn.decomposition import TruncatedSVD
@@ -38,7 +38,14 @@ def trim_float(number):
         return float('{0:.3f}'.format(number))
 
 
-# TODO: Add more measures
+class PMI:
+    """ Pointwise Mutual Information (Church & Hanks 1990). 
+    Score orientation is -log p(a,b) > 0 > -inf """
+    minimum = -math.inf
+
+    @staticmethod
+    def score(ab, a, b, cz):
+        return _log(ab*cz) - _log(a*b)
 
 class PPMI:
     """ Positive Pointwise Mutual Information.
@@ -48,6 +55,26 @@ class PPMI:
     @staticmethod
     def score(ab, a, b, cz):
         return max(_log(ab*cz) - _log(a*b), 0)
+
+class NPMI:
+    """ Normalized PMI (Bouma 2009).
+    Score orientation is  +1 > 0 > -1 """
+    minimum = -1.0
+
+    @staticmethod
+    def score(ab, a, b, cz):
+        return PMI.score(ab, a, b, cz) / -_log(ab/cz)
+
+class PMI2:
+    """ PMI^2 (Daille 1994). Fixes the low-frequency bias 
+    of PMI and NPMI by squaring the numerator to compensate 
+    multiplication done in the denominnator.
+    Scores are oriented as: 0 > log p(a,b) > -inf """
+    minimum = -math.inf
+    
+    @staticmethod
+    def score(ab, a, b, cz):
+        return PMI.score(ab, a, b, cz) - (-_log(ab/cz))
 
 
 class PMI2VEC:
@@ -61,12 +88,14 @@ class PMI2VEC:
         self.words2 = []
         self.index_table = {}
         self.word_vectors = {}
+        self.translations = {}
         
     def _read_file(self, filename):
         """ Read corpus and buffer each line to prevent skipgrams
         being calulated over given constraints (e.g. paragraphs);
 
         Input should be already tokenized! """
+        st = time.time()
 
         print("> reading %s..." % filename)
         with open(filename, 'r', encoding='utf-8',
@@ -85,6 +114,8 @@ class PMI2VEC:
         self.morph_freqs = Counter(morphs)
         self.corpus_size = len(self.text) - (buffers * self.window_size)
         self.word_freqs = Counter(self.text)
+        et = time.time() - st
+        print('> readfile (s)', et)
 
     def _is_wordofinterest(self, word, index):
         return word in self.words[index]
@@ -103,16 +134,23 @@ class PMI2VEC:
             #json.dump(self.word_vectors, data)
             data.write(json.dumps(self.word_vectors).encode('utf-8'))
 
+    def load_dictionary(self, filename):
+        with open(filename, 'r', encoding="utf-8") as data:
+            for line in data.read().splitlines():
+                if line:
+                    word, translation = line.split('\t')
+                    self.translations[word] = translation
+
     def scale(self, bigram_freq):
         """ Scale frequencies according to window size as in
         Church & Hanks 1990 """
         if not self.symmetry:
             return bigram_freq / (self.window_size - 1)
         else:
-            return bigram_freq / ((self.window_size - 1) * 2)
+            return bigram_freq / (self.window_size - 1)
         
     def score_bigrams(self, filename, measure, windowsize=2,
-                      symmetry=False, words='all'):
+                      symmetry=False, minfreq=2, words='all'):
         """ This function finds and scores bigrams (or skipgrams) from
         the given corpus file by using given measure """
         self.symmetry = symmetry
@@ -127,22 +165,24 @@ class PMI2VEC:
             for w in zip(*[self.text[i:] for i in range(1+wz*2)]):
                 #if w[wz] in self.keywords:
                 for bigram in itertools.product([w[wz]], w[0:wz]+w[wz+1:]):
-                    yield bigram
+                    if self.word_freqs[bigram[-1]] > minfreq:
+                        yield bigram
 
         def get_bigrams_forward():
             """ Asymmetric window """
             for w in zip(*[self.text[i:] for i in range(self.window_size)]):
                 #if w[0] in self.keywords:
                 for bigram in itertools.product([w[0]], w[1:]):
-                    yield bigram
+                    if self.word_freqs[bigram[-1]] > minfreq:
+                        yield bigram
 
         def get_subunits(bigrams):
             """ Count morphemic bigrams """
             for w1, w2 in bigrams:
-                for m1 in w1.split('+'):
-                    for m2 in w2.split('+'):
-                        yield (m1, m2)
-            
+                for subunit_bigram in itertools.product(w1.split('+'), w2.split('+')):
+                    yield subunit_bigram
+       
+        st = time.time()
         if symmetry:
             bigrams = get_bigrams_symmetric()
             morphs = Counter(get_subunits(bigrams))
@@ -174,9 +214,14 @@ class PMI2VEC:
                     self.scored[w1][w2] = data
                 finally:
                     pass
+
+        et = time.time() - st
+        print('> scoring (s)', et)
                 
-    def build_vectors(self, value, dimension=300, algorithm='arpack'):
+    def build_vectors(self, value, dimension=300, algorithm='arpack', precombine=False):
         """ Initialize minimum scores for the matrix """
+        st = time.time()
+
         if value == 'score':
             min_ = self.minimum
         else:
@@ -198,58 +243,78 @@ class PMI2VEC:
                 matrix.append(row)
                 i += 1
         
+        et = time.time() - st
+        print('> building sparse matrix (s)', et)
+
+        st = time.time()
         matrix = np.matrix(matrix)
         print("> truncating dimensions", matrix.shape, '->', (matrix.shape[0], dimension))
         svd = TruncatedSVD(n_components=dimension, algorithm=algorithm)
         matrix = svd.fit_transform(matrix)
 
-        """ Combine wordform vectors from morph vectors """
-        for word in self.word_freqs.keys():
-            vecs = []
-            for morph in word.split('+'):
+        """ Combine wordform vectors from morph vectors if ´precombine´
+        is set True """
+        if precombine:
+            for word in self.word_freqs.keys():
+                vecs = []
+                for morph in word.split('+'):
+                    index = self.index_table.get(morph, None)
+                    if index is not None:
+                        vecs.append(matrix[index])
+                if vecs:
+                    self.word_vectors[word.replace('+', '')] = (self.word_freqs[word],
+                                                                list(np.mean(vecs, axis=0)))
+        else:
+            for morph in self.morph_freqs.keys():
                 index = self.index_table.get(morph, None)
                 if index is not None:
-                    vecs.append(matrix[index])
-            if vecs:
-                self.word_vectors[word] = list(np.mean(vecs, axis=0))
+                    self.word_vectors[morph] = (self.morph_freqs[morph], list(matrix[index]))
 
-    def cosine_similarity(self, word1, word2):
+        et = time.time() - st
+        print('> svd (s)', et)
+
+    def cosine_similarity(self, word1, word2, minfreq=0):
         """ Compare cosine similarity between two words """
-        vec1 = self.word_vectors.get(word1, None)
-        vec2 = self.word_vectors.get(word2, None)
-        if all((vec1, vec2)):
-            return np.dot(vec1, vec2 ) / (norm(vec1) * norm(vec2))
+        freq1, vec1 = self.word_vectors.get(word1, None)
+        freq2, vec2 = self.word_vectors.get(word2, None)
+        if all((vec1, vec2)) and freq2 > minfreq:
+            return np.dot(vec1, vec2) / (norm(vec1) * norm(vec2))
 
-    def get_best(self, word):
+    def get_best(self, word, minfreq=0, amount=15):
         table = []
         for word2 in self.word_vectors.keys():
-            similarity = self.cosine_similarity(word, word2)
-            table.append((similarity, word2, dictionary.dct.get(word2, None)))
+            similarity = self.cosine_similarity(word, word2, minfreq)
+            if similarity is not None:
+                table.append((similarity, word2, self.translations.get(word2, None)))
 
-        t = sorted(table, reverse=True)
-        for x in t[:15]:
-            print(x)
+        for entry in sorted(table, reverse=True)[0:amount]:
+            print(entry)
 
-    
 def demo():
     """ How to build and save vectors from new text """
     a = PMI2VEC()
-    txt = 'Tiglath-pileserSargon_texts'
-    a.score_bigrams(txt, measure=PPMI, windowsize=5, symmetry=True)
+    #txt = 'testi.txt'
+    #txt = 'MeBo-123.2015_stamd_sunder_vokaallangde.txt'
+    #txt = 'MeBo-123.2015_stamd.txt'
+    #txt = 'MeBo-123.2015_segmenteerd.txt'
+    txt = 'noSynonyms_text_Mar19'
+    a.score_bigrams(txt, measure=PPMI, windowsize=5, symmetry=True, minfreq=2)
     a.build_vectors('score', dimension=300, algorithm='arpack')
-    a.save_vectors('tiglath.vec.gzip')
+    a.save_vectors('akkadi.vec.gzip')
 
-def demo2():
+def query():
     """ How to load vectors and test similarities """
     a = PMI2VEC()
-    a.load_vectors('tiglath.vec.gzip')
-    a.get_best('nakru')
-    #a.cosine_similarity('bēlu', 'šarru')       # lord and king: very high similarity
-    #a.cosine_similarity('Akkad_P', 'Šumeru_P') # Sumer and Akkad: high similarity
-    #a.cosine_similarity('kaspu', 'hurāṣu')     # silver and gold: high similarity
-    #a.cosine_similarity('abu', 'māru')         # father and son: medium-low similarity
-    #a.cosine_similarity('kakku', 'qabû')       # weapon and speak: very low similarity
+    a.load_dictionary('words_noSynonymsMar19')
+    a.load_vectors('akkadi_PMI.vec.gzip')
+    for emotion in ['râmu_1','adāru_2','galātu_1',
+                    'palāhu_1','parādu_1','šahātu_1',
+                    'agāgu_1','ezēzu_1','kamālu_1',
+                    'labābu_1','raʾābu_1','šabāsu_1',
+                    'šamāru_1','zenû_1']:
+        print('\n')
+        print(emotion)
+        a.get_best(emotion, minfreq=5, amount=10)
 
 #demo()
-demo2()    
-            
+query()    
