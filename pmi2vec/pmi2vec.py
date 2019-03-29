ef5751ab8532a5ecdf4527ac53d21eaddc3d14b7
Aleksi Sahala <aleksi.sahala@helsinki.fi>
2019-03-29 18:37:21 +0200

    w2v text format

--- pmi2vec/pmi2vec.py
+++ pmi2vec/pmi2vec.py
@@ -134,6 +134,20 @@ class PMI2VEC:
             #json.dump(self.word_vectors, data)
             data.write(json.dumps(self.word_vectors).encode('utf-8'))
 
+    def save_vectors_txt(self, filename):
+        """ Default Word2vec vector (text) format """
+        vector_n = len(self.word_vectors.keys())
+        with open(filename, 'w', encoding='utf-8') as data:
+            # First write number of words and dimension
+            data.write('%i %i\n' % (vector_n, self.dimension))
+            for key, value in self.word_vectors.items():
+                # Then for every vector print word and the vector
+                # each float separated by space value[0] = word freq
+                # value[-1] vector
+                data.write(key + ' ')
+                value[-1].tofile(data, sep=' ')
+                data.write('\n')
+
     def load_dictionary(self, filename):
         with open(filename, 'r', encoding="utf-8") as data:
             for line in data.read().splitlines():
@@ -226,6 +240,7 @@ class PMI2VEC:
         """ ´value´ can be either ´score´ (that is the
         association measure) or ´bigram_freq´ """
         st = time.time()
+        self.dimension = dimension
 
         if value == 'score':
             min_ = self.minimum
@@ -242,7 +257,7 @@ class PMI2VEC:
                     if w2 in self.scored[w1].keys():
                         bigram = self.scored[w1][w2]
                         score = trim_float(bigram[value])
-                row.append(score)
+                    row.append(np.float32(score))
             if any(row):
                 self.index_table[w1] = i
                 matrix.append(row)
@@ -268,12 +283,12 @@ class PMI2VEC:
                         vecs.append(matrix[index])
                 if vecs:
                     self.word_vectors[word.replace('+', '')] = (self.word_freqs[word],
-                                                                list(np.mean(vecs, axis=0)))
+                                                                np.array(np.mean(vecs, axis=0)))
         else:
             for morph in self.morph_freqs.keys():
                 index = self.index_table.get(morph, None)
                 if index is not None:
-                    self.word_vectors[morph] = (self.morph_freqs[morph], list(matrix[index]))
+                    self.word_vectors[morph] = (self.morph_freqs[morph], np.array(matrix[index]))
 
         et = time.time() - st
         print('> svd (s)', et)
@@ -282,7 +297,7 @@ class PMI2VEC:
         """ Compare cosine similarity between two words """
         freq1, vec1 = self.word_vectors.get(word1, None)
         freq2, vec2 = self.word_vectors.get(word2, None)
-        if all((vec1, vec2)) and freq2 > minfreq:
+        if all((vec1, vec2)) and freq2 >= minfreq:
             return np.dot(vec1, vec2) / (norm(vec1) * norm(vec2))
 
     def get_best(self, word, minfreq=0, amount=15):
@@ -303,9 +318,11 @@ def demo():
     #txt = 'MeBo-123.2015_stamd.txt'
     #txt = 'MeBo-123.2015_segmenteerd.txt'
     txt = 'noSynonyms_text_Mar19'
-    a.score_bigrams(txt, measure=PPMI, windowsize=5, symmetry=True, minfreq=2)
+    a.score_bigrams(txt, measure=PPMI, windowsize=5, symmetry=True, minfreq=1)
     a.build_vectors('score', dimension=300, algorithm='arpack')
-    a.save_vectors('akkadi.vec.gzip')
+    #a.save_vectors('akkadi2_bfsqrt.vec.gzip')
+    #a.save_vectors('test.vec.gzip')
+    a.save_vectors_txt('akkadikoe.vec')
 
 def query():
     """ How to load vectors and test similarities; use load_dictionary()
@@ -320,15 +337,26 @@ def query():
     
     a = PMI2VEC()
     a.load_dictionary('words_noSynonymsMar19')
-    a.load_vectors('akkadi_PMI.vec.gzip')
-    for emotion in ['râmu_1','adāru_2','galātu_1',
-                    'palāhu_1','parādu_1','šahātu_1',
-                    'agāgu_1','ezēzu_1','kamālu_1',
-                    'labābu_1','raʾābu_1','šabāsu_1',
-                    'šamāru_1','zenû_1']:
+    a.load_vectors('akkadi.vec.gzip')
+    #words = ['kakku_1', 'niāku_1', 'asakku_1', 'ikkibu_1', 'anzillu_1', 'wardatu_1', 'utukku_1','šarāqu_1', 'qaštu_1',]
+    #words = ["ennettu_1","gillatu_1","gullultu_1","gullulu_1","pippilû_1",
+    #        "šettu_1","šērtu_1","arnu_1","hiṭītu_1","hīṭu_1"]
+    #['râmu_1','adāru_2','galātu_1','palāhu_1','parādu_1','šahātu_1','agāgu_1','ezēzu_1','kamālu_1','labābu_1','raʾābu_1','šabāsu_1','šamāru_1','zenû_1']
+    words = ['sisû_1', 'qaštu_1']
+    for emotion in words[-4:]:
         print('\n')
         print(emotion)
-        a.get_best(emotion, minfreq=5, amount=10)
+        a.get_best(emotion, minfreq=3, amount=25)
 
-#demo()
-query()    
+def test():
+    a = PMI2VEC()
+    a.load_dictionary('words_noSynonymsMar19')
+    a.load_vectors('akkadi.vec.gzip')
+    words = ['immeru_1', 'alpu_1', 'sisû_1']
+    for word1 in words:
+        for word2 in words:
+            print(word1, word2, a.cosine_similarity(word1, word2))
+        
+demo()
+#query()
+#test()    
